18ec5ea230ad28d10220b5cafa7147937a84c31e
Tankiso Montse <tmontse16@gmail.com>
2025-03-25 14:20:34 +0200

    added averia-serif-libre font to layout page and upgraded nextjs version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {Averia_Serif_Libre } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const averiaSerifLibre = Averia_Serif_Libre({
+  weight: "400",
+  variable: "--font-averia-serif-libre",
   subsets: ["latin"],
 });
 
@@ -25,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${averiaSerifLibre.variable} antialiased`}>
         <Header />
         <main>
           {children}
