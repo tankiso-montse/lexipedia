775c0ab8d5274699722291c1c69c638bfabc7215
Tankiso Montse <tmontse16@gmail.com>
2025-03-29 01:30:17 +0200

    removed axios package to opt for built in fetch api

--- src/app/api/lookup/route.ts
+++ src/app/api/lookup/route.ts
@@ -1,10 +1,12 @@
-import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   const url = process.env.DICTIONARY_API_URL;
   const apiKey =process.env.DICTIONARY_API_KEY;
   const searchQuery = 'fish';
-  const response = await axios.get(`${url}${searchQuery}?key=${apiKey}`);
-  // console.log("hello", response.data[0].hwi.hw);
-  return new Response(response.data[0].hwi.hw);
+  const response = await fetch(`${url}${searchQuery}?key=${apiKey}`)
+  const data = await response.json();
+
+  return NextResponse.json(data)
+  
 }
