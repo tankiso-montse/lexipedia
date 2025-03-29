c77fbdd0f38d3e08e4c94b8c39b95e4a7889c5d9
Tankiso Montse <tmontse16@gmail.com>
2025-03-29 19:32:25 +0200

    created a tailwind config file and created a custom tailwind variable for font-averia

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='p-4 bg-gray-800 font-averia-serif'>
+    <div className='p-4 bg-gray-800 font-averia'>
       <h1>Header</h1>
     </div>
   )
