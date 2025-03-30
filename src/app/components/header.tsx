e39aba91bf6a57f8b85d43d7aa99d2bb46f8eeb2
Tankiso Montse <tmontse16@gmail.com>
2025-03-30 19:25:40 +0200

    usage example of data on the frontend

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,18 @@
+import { getSearchedWord } from '@/lib/dictionary-client'
 import React from 'react'
+import { searchedWord } from '../types/apiTypes'
 
-function Header() {
+async function Header() {
+  // const word: searchedWord = await getSearchedWord()
+ 
   return (
     <div className='p-4 bg-gray-800 font-averia'>
       <h1>Header</h1>
+      {/* <div>
+        <h1>{word.word.replace("*","")}</h1>
+        <h3>{word.phonetics?.text || ''}</h3>
+        <p>{word?.meanings?.[0].definitions?.[0].definition || ''}</p>
+      </div> */}
     </div>
   )
 }
