4be7df9c46fd98801fd0aeba84905ba558f758ca
Tankiso Montse <tmontse16@gmail.com>
2025-03-30 19:23:23 +0200

    create a type for the searchedWordData

---
+++ src/app/types/apiTypes.ts
@@ -0,0 +1,16 @@
+export type searchedWord = {
+  word: string;
+  phonetics?: {
+    text?: string;
+    audio?: string;
+  };
+  meanings?: {
+    partOfSpeech: string;
+    definitions?: {
+      definition?: string;
+      example?: string;
+      synonyms?: string[];
+      antonyms?: string[];
+    }[];
+  }[];
+};
