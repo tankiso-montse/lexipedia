15089b9903095ffcd7ba87bd7474c804e149471e
Tankiso Montse <tmontse16@gmail.com>
2025-04-06 00:07:53 +0200

    added error handling for fetching word data

--- src/lib/dictionary-client.ts
+++ src/lib/dictionary-client.ts
@@ -1,34 +1,36 @@
 import { searchedWord } from "@/app/types/apiTypes";
+import Error from "next/error";
 
 export const getSearchedWord = async () => {
-  const response = await fetch(
-    process.env.NEXT_PUBLIC_HOSTNAME + `/api/search`
-  );
-  const data = await response.json();
-  if (data) {
-    const searchedTermData: searchedWord = {
-      word: data[0].word,
-      phonetics: {
-        text: data[0].phonetics[0]?.text,
-        audio: data[0].phonetics[0]?.audio,
-      },
-      meanings: [
-        {
-          partOfSpeech: data[0].meanings[0].partOfSpeech,
-          definitions: [
-            {
-              definition: data[0].meanings[0].definitions[0].definition,
-              example: data[0].meanings[0].definitions[0].example, 
-              synonyms: data[0].synonyms,
-              antonyms: data[0].antonyms,
-            },
-          ],
+  try {
+    const response = await fetch(process.env.NEXT_PUBLIC_HOSTNAME + `/api/search`);
+    const data = await response.json();
+    if (data) {
+      const searchedTermData: searchedWord = {
+        word: data[0].word,
+        phonetics: {
+          text: data[0].phonetics[0]?.text,
+          audio: data[0].phonetics[0]?.audio,
         },
-      ],
-    };
-
-    return searchedTermData;
+        meanings: [
+          {
+            partOfSpeech: data[0].meanings[0].partOfSpeech,
+            definitions: [
+              {
+                definition: data[0].meanings[0].definitions[0].definition,
+                example: data[0].meanings[0].definitions[0].example, 
+                synonyms: data[0].synonyms[0],
+                antonyms: data[0].antonyms,
+              },
+            ],
+          },
+        ],
+      };
+  
+      return searchedTermData;
+    } 
+    return 'Word not found';
+  } catch {
+    console.error('Error fetching word data:', Error);
   }
-
-  return data;
 };
