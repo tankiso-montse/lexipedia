035b0be54a35b207e38c7d49ff91e5479500439e
Tankiso Montse <tmontse16@gmail.com>
2025-04-05 21:39:12 +0200

    updated server function to use new api route and map accurate data

--- src/lib/dictionary-client.ts
+++ src/lib/dictionary-client.ts
@@ -1,29 +1,34 @@
 import { searchedWord } from "@/app/types/apiTypes";
 
 export const getSearchedWord = async () => {
-  const response = await fetch(process.env.NEXT_PUBLIC_HOSTNAME + `/api/lookup`)
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_HOSTNAME + `/api/search`
+  );
   const data = await response.json();
   if (data) {
-    const headWord = data[0].hwi.hw.replace(/[^a-zA-Z]/g, "");
     const searchedTermData: searchedWord = {
-      word: headWord,
+      word: data[0].word,
       phonetics: {
-        text: data[0].hwi.prs[0].mw || "",
-        audio: data[0].hwi.prs[0].audio || "",
+        text: data[0].phonetics[0]?.text,
+        audio: data[0].phonetics[0]?.audio,
       },
       meanings: [
         {
-          partOfSpeech: data[0].fl,
-          definitions: [ {
-            definition: data[0].shortdef[0],
-          }
-          ]
-        }
-      ]
-    }
+          partOfSpeech: data.meanings[0].partOfSpeech,
+          definitions: [
+            {
+              definition: data.meanings[0].definitions[0].definition,
+              example: data.meanings[0].definitions[0].example, 
+              synonyms: data.synonyms[0],
+              antonyms: data.antonyms[0],
+            },
+          ],
+        },
+      ],
+    };
 
     return searchedTermData;
   }
 
   return data;
-}
+};
