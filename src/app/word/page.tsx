054470a53bea3f746f67781ba35f4c9df452468b
Tankiso Montse <tmontse16@gmail.com>
2025-04-14 08:56:14 +0200

    got the right colour for the background, need to work on moving the searchbar component to the front

--- src/app/word/page.tsx
+++ src/app/word/page.tsx
@@ -3,6 +3,7 @@
 import { Search } from "lucide-react";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import SearchBar from "../components/SearchBar";
 
 export default function SearchPage() {
   const router = useRouter();
@@ -15,7 +16,14 @@ export default function SearchPage() {
 
   return (
     <div className="bg-secondary opacity-40">
-      <div className="bg-[url('/search-bg.jpeg')] bg-cover w-full h-screen opacity-20">
+      <div className="bg-[url('/search-bg.jpeg')] bg-cover w-full h-screen opacity-20 grid grid-rows-[20%_80%] -z-10">
+        <div className="flex items-center justify-center z-50">
+          <div className="w-2xl relative bg-white rounded-full px-4 py-2">
+            <input type="text" placeholder="Search for word" className="bg-white"/>
+          </div>
+        </div>
+        <div className="bg-cyan-400"></div>
+
       </div>
     </div>
   );
