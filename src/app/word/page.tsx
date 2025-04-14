4a9ce062d0cf939e29a219e5991662a5e93bc5f0
Tankiso Montse <tmontse16@gmail.com>
2025-04-14 09:40:17 +0200

    removed temporary to fix layout

--- src/app/word/page.tsx
+++ src/app/word/page.tsx
@@ -16,7 +16,7 @@ export default function SearchPage() {
 
   return (
     <div className="bg-secondary opacity-40">
-      <div className="bg-[url('/search-bg.jpeg')] bg-cover w-full h-screen opacity-20 grid grid-rows-[20%_80%] -z-10">
+      <div className="bg-[url('/search-bg.jpeg')] bg-cover w-full h-screen opacity-20 -z-10">
         <div className="flex items-center justify-center z-50">
           <div className="w-2xl relative bg-white rounded-full px-4 py-2">
             <input type="text" placeholder="Search for word" className="bg-white"/>
