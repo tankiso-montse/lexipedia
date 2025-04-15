70726b09096cbcdbf0a1ff2344aef087473407f1
Tankiso Montse <tmontse16@gmail.com>
2025-04-15 21:22:27 +0200

    used tailwind colours for input/placeholder text and applied colours that match figma designs

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -15,22 +15,16 @@ export default function SearchBar() {
   };
 
   return (
-    <div className="flex items-center rounded-full px-4 py-2 w-[500px] shadow-lg z-10">
-      {/* Search Icon */}
-      <div className="text-[rgba(244,244,244,0.88)] mr-2">
-        <Search size={20} />
-      </div>
-
-      {/* Input Field */}
+    <div className="flex items-center rounded-full px-4 py-2 w-[500px] shadow-lg z-10 bg-searchBar">
+        <Search size={20} className="text-white/40" />
+   
       <input
         type="text"
         placeholder="Search for a word"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="flex-grow outline-none text-[rgba(244,244,244,0.88)] placeholder-[rgba(187,187,187,0.88)]"
+        className="flex-grow outline-none text-white/80 placeholder-white/40 ml-3 text-xl"
       />
-
-      {/* Curved Search Button */}
       <button
         onClick={handleSearch}
         className="w-[100px] ml-2 bg-amber-500 text-white px-4 py-2 rounded-full hover:bg-[#d3740e] transition"
