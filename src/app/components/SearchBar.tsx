ede4f27ea5d5b26103419e9c4482e4bbac7b7a3a
Tankiso Montse <tmontse16@gmail.com>
2025-04-16 19:58:54 +0200

    made search bar width mobile responsive

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -15,7 +15,7 @@ export default function SearchBar() {
   };
 
   return (
-    <div className="flex items-center rounded-full px-4 py-2 w-1/4 shadow-lg bg-searchBar border border-amber-200">
+    <div className="flex items-center rounded-full px-4 py-2 sm:w-1/2 md:w-1/3 lg:w-1/4 shadow-lg bg-searchBar border border-amber-200">
         <Search size={20} className="text-white/40" />
    
       <input
