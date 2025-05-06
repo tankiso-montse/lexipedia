cb83cc488141fde22524272c374c05390a1f5523
sngwenyama023 <siyabongangwenyama22@gmail.com>
2025-05-06 23:40:34 +0200

    updated changes on landing page and search page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,39 +6,35 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <main>
-      <div>
-        {/* Background Image */}
-        <div className="h-screen bg-cover bg-[url('/bg-landing-page.png')] bg-no-repeat"></div>
+    <main className="relative h-screen w-full bg-cover bg-center bg-[url('/bg-landing-page.png')] bg-no-repeat">
+        <div className="h-screen w-full bg-black/50 flex flex-col items-center justify-center">
+          <div className="h-1/2 w-1/2 ">
+            <div className="font-averia font-bold text-center ">
+                <h1 className="mt-4 mb-6 text-9xl tracking-[0.9rem]  text-white uppercase">
+                  Lexipedia
+                </h1>
 
-        {/* Dark Overlay */}
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-
-        {/* Centered Content */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center">
-          <h1 className="w-[547px] h-[52px] font-averia text-9xl font-bold leading-[1.2] tracking-[0.1em] text-center text-white uppercase mt-[20px] mr-90">
-            Lexipedia
-          </h1>
-
-          <h2 className="w-[547px] h-[52px] font-averia text-3xl font-bold leading-[230px] tracking-[0px] text-center text-amber-300 mt-12">
-            Explore the depths of language
-          </h2>
-
-          <button
-          onClick={() => router.push('/search')}
-            className="
-              w-[200px] h-[70px] mt-45
-              bg-amber-500  text-white  leading-[1.2] text-3xl font-averia
-              rounded-[2rem]
-              hover:scale-105 transition duration-200
-              flex items-center justify-center gap-2
-            "
-          >
-            Discover <ArrowRight size={32} />
-          </button>
+                <h2 className=" text-3xl tracking-[0.3rem] text-amber-300">
+                  Explore the depths of language
+                </h2>
+            </div>
+            <div className=" h-1/2 w-full pt-35 flex items-center justify-center " >
+                <button
+                onClick={() => router.push('/search')}
+                  className="
+                    w-42 h-16
+                    bg-amber-500  text-white text-3xl font-averia
+                    rounded-[2rem]
+                    hover:scale-105 transition duration-200
+                    flex items-center justify-center gap-2
+                  "
+                >
+                  Discover <ArrowRight size={32} />
+                </button>
+            </div>
+          </div>
 
         </div>
-      </div>
     </main>
   );
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -13,31 +13,11 @@ export default function SearchPage() {
 
       <div className="bg-secondary h-full">
         <div className=" flex items-center justify-center z-10 h-1/6 ">
-          
-
-
-        <div className="flex items-center rounded-full bg-[rgba(255,255,255,0.22)]  px-4 py-2 w-[500px] shadow-lg border-2 ">
-
-       
-         <div className="text-[rgba(244,244,244,0.88)] mr-2">
-           <Search size={20} />
-         </div>
-
-      
-         <input
-          type="text"
-          placeholder="Search for a word"
-          className="flex-grow bg-transparent outline-none text-[rgba(244,244,244,0.88)] placeholder-[rgba(187,187,187,0.88)] "
-        />
-
-        {/* Curved Search Button */}
-      <button onClick={() => router.push('/word')}  className="w-[100px] bg-[#804a04] text-white px-4 py-2 rounded-full hover:bg-[#d3740e] transition">
-          Search
-        </button>
-    </div>
+          <SearchBar></SearchBar>
         </div>
         <div className="h-5/5 flex gap-4 absolute w-5/6 right-0 text-textColor ">
-          <div className="h-6/8 w-0.5 bg-[#938048] ml-40"></div>
+          <div className="h-6/8 w-0.5 bg-amber-200 ml-40"></div>
+          {/* bg-[#938048]  colour for text */}
           <div className="flex pt-45 pl-20 flex-col gap-5">
           <span className="text-7xl">Type a word to lookup</span>
           <span className="text-4xl pl-23 pt-0">Lexipedia will reveal its meaning</span>
