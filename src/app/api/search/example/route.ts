58e185b532702c0c80f1ce112454396e29a47cfb
Tankiso Montse <tmontse16@gmail.com>
2025-05-12 19:54:59 +0200

    route file for example endpoint

---
+++ src/app/api/search/example/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const searchQuery = 'people';
+  const response = await fetch(`https://api.wordnik.com/v4/word.json/${searchQuery}/topExample?useCanonical=false&api_key=${process.env.WORDNIK_API_KEY}`);
+  if (!response.ok) {
+    return NextResponse.json({ error: 'Failed to fetch word example data' }, { status: 500 });
+  }
+  const data = await response.json();
+  return NextResponse.json(data);
+}
