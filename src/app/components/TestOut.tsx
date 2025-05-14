d650d8dfafe6e9edf3f9a1e7b9a8b0c90164e68f
Tankiso Montse <tmontse16@gmail.com>
2025-05-14 12:35:23 +0200

    wrote a server action for relatedWords endpoint

--- src/app/components/TestOut.tsx
+++ src/app/components/TestOut.tsx
@@ -1,14 +1,14 @@
-import { getWordAudio, getWordDefinition, getWordExample } from '@/services/dictionaryService'
+import { getPronounciation, getWordAudio, getWordDefinition, getWordExample } from '@/services/dictionaryService'
 import React from 'react'
 
 async function TestOut() {
-  const [wordDefinition, wordExample, wordAudio] = await Promise.all([getWordDefinition(), getWordExample(), getWordAudio()])
+  const [wordDefinition, wordExample, wordAudio, wordPronounciation] = await Promise.all([getWordDefinition(), getWordExample(), getWordAudio(), getPronounciation()])
 
   return (
     <>
     <div className='flex flex-col items-center justify-center gap-4'>
       <h1>{wordDefinition?.word}</h1>
-
+      <h3>{wordPronounciation?.pronounciation}</h3>
       <span>{wordDefinition?.partOfSpeech}</span>
       <h3>{wordDefinition?.definition}</h3>
       <p>{wordExample?.example}</p>
--- src/services/dictionaryService.ts
+++ src/services/dictionaryService.ts
@@ -1,42 +1,53 @@
-import { wordAudio, wordDefinition, wordExample } from "@/app/types/apiTypes";
+import {
+  relatedWords,
+  wordAudio,
+  wordDefinition,
+  wordExample,
+  wordPronounciation,
+} from "@/app/types/apiTypes";
 
 export async function getWordDefinition() {
-  const response = await fetch(process.env.NEXT_PUBLIC_HOSTNAME + `/api/search/definitions`)
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_HOSTNAME + `/api/search/definitions`
+  );
   try {
     const data = await response.json();
-    if (data.length >= 1 ) {
+    if (data.length >= 1) {
       for (let i = 0; i < data.length; i++) {
         if (data[i].text) {
           const wordDefinition: wordDefinition = {
             word: data[i].word,
             partOfSpeech: data[i].partOfSpeech,
             definition: data[i].text,
-          }
+          };
           return wordDefinition;
         }
       }
     }
-} catch (error) {
-    console.error('Error fetching word definition data:', error);
+  } catch (error) {
+    console.error("Error fetching word definition data:", error);
   }
 }
 
 export async function getWordExample() {
-  const response = await fetch(process.env.NEXT_PUBLIC_HOSTNAME + `/api/search/example`)
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_HOSTNAME + `/api/search/example`
+  );
   try {
     const data = await response.json();
     const wordExample: wordExample = {
       example: data.text,
-    }
+    };
     return wordExample;
   } catch (error) {
-    console.error('Error fetching word example data:', error);
+    console.error("Error fetching word example data:", error);
   }
 }
 
-
 export async function getWordAudio() {
-  const response = await fetch(process.env.NEXT_PUBLIC_HOSTNAME + `/api/search/audio`)
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_HOSTNAME + `/api/search/audio`
+  );
   try {
     const data = await response.json();
     if (data.length > 1) {
@@ -51,6 +62,53 @@ export async function getWordAudio() {
       return wordAudio;
     }
   } catch (error) {
-    console.error('Error fetching word audio data:', error);
+    console.error("Error fetching word audio data:", error);
+  }
+}
+
+export async function getPronounciation() {
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_HOSTNAME + `/api/search/pronounciations`
+  );
+  try {
+    const data = await response.json();
+    if (data.length > 0) {
+      const wordPronounciation: wordPronounciation = {
+        pronounciation: data[0].raw,
+      };
+      return wordPronounciation;
+    }
+  } catch (error) {
+    console.error("Error fetching word pronounciation data:", error);
+  }
+}
+
+export async function getRelatedWords() {
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_HOSTNAME + `/api/search/related-words`
+  );
+  try {
+    const data = await response.json();
+    if (data.length > 0) {
+      for (let object = 0; object < data.length; object++) {
+        if (
+          data[object].relationshipType === "synonym" ||
+          data[object].relationshipType === "antonym"
+        ) {
+          const relatedWords: relatedWords = {
+            antonyms: data[object].words,
+            synonyms: data[object].words,
+          };
+          return relatedWords;
+        } else {
+          return {
+            antonyms: [],
+            synonyms: [],
+          }
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error fetching related words data:", error);
   }
 }
