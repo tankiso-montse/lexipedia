4d41c685b6fb83e0f9d1bd026e090545e345a929
Tankiso Montse <tmontse16@gmail.com>
2025-05-20 03:30:42 +0200

    created a dynamic route for definitions

--- src/app/api/search/definitions/[word]/route.ts
+++ src/app/api/search/definitions/[word]/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const searchQuery = "aggressive";
+export async function GET({params}: {params: {word: string}}) {
+  const { word } = params;
   const response = await fetch(
-    `https://api.wordnik.com/v4/word.json/${searchQuery}/definitions?limit=3&includeRelated=false&sourceDictionaries=all&api_key=${process.env.WORDNIK_API_KEY}`
+    `https://api.wordnik.com/v4/word.json/${word}/definitions?limit=3&includeRelated=false&sourceDictionaries=all&api_key=${process.env.WORDNIK_API_KEY}`
   );
   if (!response.ok) {
     return NextResponse.json(
